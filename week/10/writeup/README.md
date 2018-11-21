5446a00194d92859305cacf185a13306703d87df
rziemski <rziemski@umd.edu>
2018-11-21 13:28:04 -0500

    completed assignment 10

--- week/10/writeup/README.md
+++ week/10/writeup/README.md
@@ -1,18 +1,24 @@
 Writeup 10 - Crypto II
 =====
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION HERE*
+Name: *Ryan Ziemski*
+Section: *0201*
 
 I pledge on my honor that I have not given or received anyunauthorized assistance on this assignment or examination.
 
-Digital acknowledgement of honor pledge: *PUT YOUR NAME HERE*
+Digital acknowledgement of honor pledge: *Ryan Ziemski*
 
 ## Assignment 10 Writeup
 
 ### Part 1 (70 Pts)
-
+*Original Hash: ac06bd7b0c41e7b6544fd014751666d9*
+*Crafted Hash: 38086ea35ddd93077800e5e4e54a23e3*
+*Payload: 'Hello World!\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\\xb0\x00\x00\x00\x00\x00\x00\x00Something malicious'
+Flag: CMSC389R-{i_still_put_the_M_between_the_DV}*
 
 ### Part 2 (30 Pts)
-
+*To generate a key:* `gpg --gen-key`
+*Then you type in a name, email, and a password*
+*To import a public key:* `gpg --import pubkey.asc`
+*To encrypt a message and dump it to a file:* `gpg -e -u "Your Name" -r "Their Name" msg.txt`
 
--- week/10/writeup/stub.py
+++ week/10/writeup/stub.py
@@ -1,19 +1,42 @@
 #!/usr/bin/env python2
 # from the git repo
 import md5py
+import socket
+import time
 
 #####################################
 ### STEP 1: Calculate forged hash ###
 #####################################
 
-message = ''    # original message here
-legit = ''      # a legit hash of secret + message goes here, obtained from signing a message
+host = "142.93.118.186"
+port = 1234
+
+#connecting to notary
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+data = s.recv(1024)
+print(data)
+
+#choosing option 1
+s.send('1\n')
+data = s.recv(1024)
+print(data)
+
+#sending 'Hello World!' as message
+s.send('Hello World!\n')
+data = s.recv(1024)
+print(data)
+data = data.split(':')
+
+
+message = 'Hello World!'    # original message here
+legit = data[2].strip()      # a legit hash of secret + message goes here, obtained from signing a message
 
 # initialize hash object with state of a vulnerable hash
 fake_md5 = md5py.new('A' * 64)
 fake_md5.A, fake_md5.B, fake_md5.C, fake_md5.D = md5py._bytelist2long(legit.decode('hex'))
 
-malicious = ''  # put your malicious message here
+malicious = 'Something malicious'  # put your malicious message here
 
 # update legit hash with malicious message
 fake_md5.update(malicious)
@@ -35,7 +58,22 @@ print(fake_hash)
 # (i.e. 20 char msg = 160 bits = 0xa0 = '\xa0\x00\x00\x00\x00\x00\x00\x00\x00')
 # craft padding to align the block as MD5 would do it
 # (i.e. len(secret + message + padding) = 64 bytes = 512 bits
-padding = ''
+msg_len_in_bytes = len(message)
+msg_len_in_bits = msg_len_in_bytes * 8
+pad_len_in_bytes = ((448 - msg_len_in_bits) / 8) - 1 #subtract one bc of \x80 in beginning
+secret_len = 10   # this is a guess, should be between 6 and 15
+
+padding = '\x80'
+count = pad_len_in_bytes - secret_len
+while count != 0:
+	padding = padding + '\x00'
+	count = count - 1
+
+padding = padding + '\\' + hex(msg_len_in_bits + (secret_len * 8))[1::] # size of message + secret in bits
+count = 7
+while count != 0:
+	padding = padding + '\x00'
+	count = count - 1 
 
 # payload is the message that corresponds to the hash in `fake_hash`
 # server will calculate md5(secret + payload)
@@ -45,3 +83,25 @@ payload = message + padding + malicious
 
 # send `fake_hash` and `payload` to server (manually or with sockets)
 # REMEMBER: every time you sign new data, you will regenerate a new secret!
+
+#send second option
+s.send('2\n')
+data = s.recv(1024)
+print(data)
+
+#send fake hash
+s.send(fake_hash + '\n')
+data = s.recv(1024)
+print(data)
+
+#send payload
+s.send(payload + '\n')
+time.sleep(1)
+data = s.recv(1024)
+print(data)
+print(repr(payload))
+
+s.close()
+
+
+
